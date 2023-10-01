5116d6ec4e97fbadc78d85b1bb4ed80d23e852dc
Someone-who-loves-coding <ps2284@srmist.edu.in>
2023-10-02 00:31:00 +0530

    Creating Contact Us page

--- Django/Hello_World/HomePage/urls.py
+++ Django/Hello_World/HomePage/urls.py
@@ -4,5 +4,6 @@ from HomePage import views
 
 urlpatterns = [
     path('', views.Home , name='HomePage' ),
-    path('About/', views.About , name='About' )
+    path('About/', views.About , name='About' ),
+    path('Contact/', views.Contact_Us , name='Contact' )
 ]
--- Django/Hello_World/HomePage/views.py
+++ Django/Hello_World/HomePage/views.py
@@ -7,5 +7,8 @@ def Home(request) :
     }
     return render(request , 'Homepage.html' , context)
     
+def Contact_Us(request) :
+    return render(request , 'Contact Us.html')
+
 def About(request) :
     return render(request , 'About.html')
