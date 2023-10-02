3202daaf1645c3269d88eb1fcfcdc0c14660d3e4
Someone-who-loves-coding <ps2284@srmist.edu.in>
2023-10-02 22:36:27 +0530

    Finalized
    Project has been Done waiting to Upload...

--- Django/Hello_World/HomePage/models.py
+++ Django/Hello_World/HomePage/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
+from datetime import datetime
 class Contact(models.Model):
     name = models.CharField(max_length=122)
     Email= models.CharField(max_length=122)
-    Phone= models.IntegerField()
-    Subject=models.CharField(max_length=200)
+    Comment=models.CharField(max_length=200)
+    Date= models.DateField(default=datetime.now())
--- Django/Hello_World/HomePage/views.py
+++ Django/Hello_World/HomePage/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render,HttpResponse
+from HomePage.models import Contact
+from datetime import datetime
 
 def Home(request) : 
-    context  = {
-        'variable1':  'Prakhar Saxena' ,
-        'variable2':  'SRM institute of Science and Technology'
-    }
-    return render(request , 'Index.html' , context)
-    
-def Contact_Us(request) :
-    return render(request , 'Contact Us.html')
-
-def About(request) :
-    return render(request , 'About.html')
+    if request.method == "POST" :
+        name = request.POST.get('name')
+        Email = request.POST.get('email')
+        comment = request.POST.get('comment')
+        contact =  Contact( name=name , Email=Email , Comment=comment , Date=datetime.today())
+        contact.save()
+    return render(request , 'Index.html')
