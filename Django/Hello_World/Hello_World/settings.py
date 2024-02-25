94c32c9ad78711df4312790d8184300aa0ccff52
Someone-who-loves-coding <ps2284@srmist.edu.in>
2024-02-26 01:16:23 +0530

    Update

--- Django/Hello_World/Hello_World/settings.py
+++ Django/Hello_World/Hello_World/settings.py
@@ -28,8 +28,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS =localhost 127.0.0.1 
-
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 # Application definition
 
